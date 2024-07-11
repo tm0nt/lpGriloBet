3e87e0fec511193d1aa42ddc9a501e4fb2b5c454
Tássio Montenegro <109485884+tm0nt@users.noreply.github.com>
2024-07-11 17:43:57 -0300

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.js
+import { defineNuxtConfig } from 'nuxt'
+
 export default defineNuxtConfig({
-  modules: ["@unocss/nuxt", "@nuxtjs/color-mode", "@nuxt/image"],
+  modules: ["@unocss/nuxt", "@nuxtjs/color-mode", "@nuxt/image", "@nuxtjs/pwa"],
   colorMode: {
-    preference: "system",
+    preference: "dark",
     fallback: "dark",
     classPrefix: "",
     classSuffix: "",
     storageKey: "app-theme",
   },
+  pwa: {
+    manifest: {
+      name: 'Parceiros - GriloBet',
+      short_name: 'GriloBet',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#00d223',
+      theme_color: '#00d223'
+    }
+  }
 });
